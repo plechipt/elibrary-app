ffdec7c42cf26b688ffedae542820ebbf0f73e09
Jakub Plecháč <loxtox74@gmail.com>
2021-03-11 16:59:52 +0100

    edited Books component

--- frontend/src/components/Books/Books.js
+++ frontend/src/components/Books/Books.js
@@ -1,7 +1,43 @@
 import React from "react";
+import "./Books.css";
+
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Grid";
 
 const Books = () => {
-  return <div>Books</div>;
+  return (
+    <div className="books-container">
+      <Grid container>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className="book" />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className="book" />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className="book" />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className="book" />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className="book" />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className="book" />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className="book" />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className="book" />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Paper className="book" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Books;
