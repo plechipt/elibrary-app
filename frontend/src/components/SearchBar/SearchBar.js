3c049874bf55242abe85500aa227ebfa203ab804
Jakub Plecháč <loxtox74@gmail.com>
2021-03-11 16:14:50 +0100

    edited SearchBar component

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,12 +1,24 @@
 import React from "react";
+import "./SearchBar.css";
 
+import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 
 const SearchBar = () => {
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    console.log();
+  };
+
   return (
-    <div>
-      <TextField id="standard-basic" label="Standard" />
-    </div>
+    <Grid className="searchbar-container" container>
+      <Grid item xs={11} sm={8} md={6} lg={4}>
+        <form onSubmit={handleOnSubmit} noValidate autoComplete="off">
+          <TextField fullWidth label="Search a book..." />
+        </form>
+      </Grid>
+    </Grid>
   );
 };
 
