12d45992eea634a138068febf5149f85840708af
Jakub Plecháč <loxtox74@gmail.com>
2021-03-12 14:35:04 +0100

    updated Books component

--- frontend/src/components/Books/Books.js
+++ frontend/src/components/Books/Books.js
@@ -3,37 +3,140 @@ import "./Books.css";
 
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const PUBLIC_FOLDER = process.env.PUBLIC_URL;
 
 const Books = () => {
   return (
     <div className="books-container">
       <Grid container>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper className="book" />
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <Paper className="book">
+            <img
+              src={`${PUBLIC_FOLDER}/static/images/hamlet.jpg`}
+              className="card-img-top"
+              alt=""
+            />
+            <Button
+              fullWidth
+              className="book-button"
+              color="primary"
+              variant="contained"
+              size="large"
+            >
+              More Info
+            </Button>
+          </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper className="book" />
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <Paper className="book">
+            <img
+              src={`${PUBLIC_FOLDER}/static/images/romeo_a_julie.jpg`}
+              className="card-img-top"
+              alt=""
+            />
+            <Button
+              fullWidth
+              className="book-button"
+              color="primary"
+              variant="contained"
+              size="large"
+            >
+              More Info
+            </Button>
+          </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper className="book" />
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <Paper className="book">
+            <img
+              src={`${PUBLIC_FOLDER}/static/images/romeo_a_julie.jpg`}
+              className="card-img-top"
+              alt=""
+            />
+            <Button
+              fullWidth
+              className="book-button"
+              color="primary"
+              variant="contained"
+              size="large"
+            >
+              More Info
+            </Button>
+          </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper className="book" />
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <Paper className="book">
+            <img
+              src={`${PUBLIC_FOLDER}/static/images/romeo_a_julie.jpg`}
+              className="card-img-top"
+              alt=""
+            />
+            <Button
+              fullWidth
+              className="book-button"
+              color="primary"
+              variant="contained"
+              size="large"
+            >
+              More Info
+            </Button>
+          </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper className="book" />
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <Paper className="book">
+            <img
+              src={`${PUBLIC_FOLDER}/static/images/romeo_a_julie.jpg`}
+              className="card-img-top"
+              alt=""
+            />
+            <Button
+              fullWidth
+              className="book-button"
+              color="primary"
+              variant="contained"
+              size="large"
+            >
+              More Info
+            </Button>
+          </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper className="book" />
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <Paper className="book">
+            <img
+              src={`${PUBLIC_FOLDER}/static/images/romeo_a_julie.jpg`}
+              className="card-img-top"
+              alt=""
+            />
+            <Button
+              fullWidth
+              className="book-button"
+              color="primary"
+              variant="contained"
+              size="large"
+            >
+              More Info
+            </Button>
+          </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper className="book" />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper className="book" />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper className="book" />
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <Paper className="book">
+            <img
+              src={`${PUBLIC_FOLDER}/static/images/romeo_a_julie.jpg`}
+              className="card-img-top"
+              alt=""
+            />
+            <Button
+              fullWidth
+              className="book-button"
+              color="primary"
+              variant="contained"
+              size="large"
+            >
+              More Info
+            </Button>
+          </Paper>
         </Grid>
       </Grid>
     </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById("root")
 );
