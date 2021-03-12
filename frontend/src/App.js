5b9ebeaed00a79dcad149508530cb00d4959c9cb
Jakub Plecháč <loxtox74@gmail.com>
2021-03-12 12:12:24 +0100

    created UserBooks component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import SearchBar from "./components/SearchBar/SearchBar";
 import Books from "./components/Books/Books";
+import UserBooks from "./components/UserBooks/UserBooks";
 
 const App = () => {
   return (
---
+++ frontend/src/components/UserBooks/UserBooks.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Books from "../Books/Books";
+
+const UserBooks = () => {
+  return <Books />;
+};
+
+export default UserBooks;
