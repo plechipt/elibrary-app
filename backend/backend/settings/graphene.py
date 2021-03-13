4a9b97e2dbea47dfc5b6324ceacedadc7f620d6c
Jakub Plecháč <loxtox74@gmail.com>
2021-03-13 11:18:01 +0100

    implemented django graphql jwt

--- backend/backend/settings/graphene.py
+++ backend/backend/settings/graphene.py
@@ -1,3 +1,11 @@
 GRAPHENE = {
     "SCHEMA": "backend.schema.schema"
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
 }
+
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
