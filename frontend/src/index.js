f7da95d0461812b4a6c1dfb6577000b2a9809811
Jakub Plecháč <loxtox74@gmail.com>
2021-03-13 22:05:02 +0100

    implemented apollo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,46 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Cookies from "js-cookie";
+import { setContext } from "@apollo/client/link/context";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const httpLink = createHttpLink({
+  uri: `${BASE_URL}/graphql/`,
+  credentials: "same-origin",
+});
+
+// Access token is send through httponly cookie
+const authLink = setContext((_, { headers }) => {
+  // Get csrftoken from Cookies
+  const csrftoken = Cookies.get("csrftoken");
+
+  // Return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      "X-CSRFToken": csrftoken,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById("root")
 );
