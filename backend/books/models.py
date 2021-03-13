0a45d571407f22d6fad370b354459b208eb524a4
Jakub Plecháč <loxtox74@gmail.com>
2021-03-13 18:24:50 +0100

    created Book objects

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     number_of_pages = models.IntegerField()
     genre = models.CharField(max_length=50)
     image_name = models.CharField(max_length=50, default='default.jpg')
+
+    def __str__(self):
+        return self.title
