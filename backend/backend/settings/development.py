c2ed47d1ab484f2c336f9eb6f171e39fdda48da4
Jakub Plecháč <loxtox74@gmail.com>
2021-03-13 11:06:23 +0100

    put settings in more files

---
+++ backend/backend/settings/development.py
@@ -0,0 +1,15 @@
+ALLOWED_HOSTS = []
+
+# Files
+ROOT_URLCONF = 'backend.urls'
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
