f08756fa93c6e845714caf82a477de924fd52ab1
Jakub Plecháč <loxtox74@gmail.com>
2021-03-15 12:58:47 +0100

    created Borrowing model

--- backend/borrowings/admin.py
+++ backend/borrowings/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Borrowing
 
-# Register your models here.
+admin.site.register(Borrowing)
+
--- backend/borrowings/models.py
+++ backend/borrowings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.OneToOneField(Book, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} borrowed {self.book.title}'
