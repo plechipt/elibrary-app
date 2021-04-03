fc74929fb7d337821cfa5fbc5f32d2fcd0f2042c
Jakub Plecháč <loxtox74@gmail.com>
2021-04-04 00:17:11 +0200

    graphiql set to False

--- backend/urls.py
+++ backend/urls.py
@@ -15,7 +15,7 @@ ADMIN_PATH = os.getenv("ADMIN_PATH")
 
 urlpatterns = [
     path(f'{ADMIN_PATH}/', admin.site.urls),
-    path("graphql/", jwt_cookie(GraphQLView.as_view(graphiql=GRAPHIQL))),
+    path("graphql/", jwt_cookie(GraphQLView.as_view(graphiql=False))),
     path('robots.txt', TemplateView.as_view(template_name='static/text/robots.txt')),
     re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
