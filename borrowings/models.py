9fa79bd4596dc6d5e6f002a35492205c8cd1624a
Jakub Plecháč <loxtox74@gmail.com>
2021-04-19 19:39:10 +0200

    created date_string field in Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -9,6 +9,7 @@ class Borrowing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.OneToOneField(Book, on_delete=models.CASCADE)
     date = models.DateTimeField(default=timezone.now)
+    date_string = models.CharField(blank=True, null=True, max_length=50)
 
     def __str__(self):
         return f'{self.user.username} borrowed {self.book.title}'
