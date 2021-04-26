112737423087f1871f95a7c36c24159195a05c6b
Jakub Plecháč <loxtox74@gmail.com>
2021-04-26 23:27:22 +0200

    resize image after upload

--- books/models.py
+++ books/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from PIL import Image
 
 
 class Book(models.Model):
@@ -20,6 +21,16 @@ class Book(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.height > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
 
 
 @receiver(post_save, sender=Book)
