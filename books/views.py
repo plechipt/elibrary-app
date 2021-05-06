b1e6e2de0cf59bbe4064655eae4e397741635af0
Jakub Plecháč <loxtox74@gmail.com>
2021-05-06 16:52:29 +0200

    created custom pagination in books app

--- books/views.py
+++ books/views.py
@@ -3,12 +3,30 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Book
 from .serializers import BookSerializer, BookFilter
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 12
+    page_size_query_param = 'page_size'
+    
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'total_pages': self.page.paginator.num_pages,
+            'count': self.page.paginator.count,
+            'results': data
+        })
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_class = BookFilter
+    pagination_class = CustomPagination
