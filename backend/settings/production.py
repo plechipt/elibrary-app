853f0d150f9d80f01f6583f708ac122b2361e13b
Jakub Plecháč <loxtox74@gmail.com>
2021-06-01 17:55:33 +0200

    implemented whitenoise

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -26,20 +26,23 @@ if DEBUG == 'FALSE':
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 
+AWS_ACCESS_KEY_ID = os.getenv('ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = os.getenv('CUSTOM_DOMAIN')
+
+AWS_LOCATION = 'static'
+#AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
 # S3 bucket config
 if USE_S3 == 'TRUE':
-    MEDIA_URL = '/media/'
+    #MEDIA_URL = '/media/'
     STATIC_URL = BUCKET_URL + '/static/'
-    MEDIA_ROOT = BUCKET_URL + '/static/images/'
-
-    AWS_ACCESS_KEY_ID = os.getenv('ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
-    AWS_S3_CUSTOM_DOMAIN = os.getenv('CUSTOM_DOMAIN')
-
-    AWS_LOCATION = 'static'
-    #AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = None
+    MEDIA_URL = BUCKET_URL + '/static/images/'
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
@@ -48,3 +51,4 @@ else:
     MEDIA_URL = '/media/'
     STATIC_URL = '/static/'
     MEDIA_ROOT = os.path.join(BASE_DIR / 'public' / 'static' / 'images')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
